94f4a9bfe26fba07fcaf0abda6c8a855c0bd173d
Gabiroba <gabrielcardosogirarde@gmail.com>
2023-12-26 18:13:52 -0300

    Adição de novos conteúdos

---
+++ MigrationsComEnvolve/Db/Populate/R__Insert_Product.sql
@@ -0,0 +1,3 @@
+INSERT INTO products ('name', 'price') VALUES 
+    ('Smartphone', 1000),
+    ('Notebook', 1500);
---
+++ MigrationsComEnvolve/Program.cs
@@ -0,0 +1,19 @@
+﻿
+using System.Data.SQLite;
+using EvolveDb;
+
+try 
+{
+    var connection = new SQLiteConnection("Data source=app.db");
+
+    var evolve = new Evolve(connection)
+    {
+        Locations = new List<string>() { "Db/Migrations", "Db/Populate" },
+    };
+
+    evolve.Migrate();
+}
+catch 
+{
+    throw;
+}
